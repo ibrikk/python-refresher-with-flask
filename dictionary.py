1651c9d1f6fb1610c24ff486bcd109a8bc66fc10
Ibrahim Khalilov <ibrahimkhalilov@Ibrahims-Mac-mini.local>
2021-07-29 22:56:31 -0400

    refresher

--- dictionary.py
+++ dictionary.py
@@ -10,3 +10,6 @@ student_attendance = {"Rolf": 96, "Bob": 80, "Anne": 100}
 
 for student in student_attendance:
   print(f"{student}: {student_attendance[student]}")
+
+for student, attendance in student_attendance.items():
+  print(f"{student}: {attendance}")
